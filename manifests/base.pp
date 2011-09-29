0d5b3eb8acf9ab894daf86c114f13ec76e976006
Howard Tyson <howard@howardtyson.com>
2011-09-29 17:36:56 -0400

    updated base.pp to run things in an order that won't break stuff

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,14 +1,34 @@
-group { 'puppet':
-  ensure => present,
+stage { 'first': before => Stage['main'] }
+
+class {
+  'setup': stage => first;
+  'vagrantvm': stage => main;
 }
 
-$mysqlpassword = 'pass'
-# passwords must always be actual hashes as used on disk in /etc/shadow
-$webadminpassword = '$6$bCysFszn$Ycq5zFTrRm8QOWKWJk9qAr94okYBVLiQm/tcWe9dT6ohuFl46i8DoWP.LMdYJG7dQyFJh0XgDGeiWhnHUHLKF.'
-$rootpassword = $webadminpassword
-$backuppassword = $webadminpassword
-include zivtechbase
-include stylomatic
-include solr
-include php53-dev
-include mysql5
+class setup {
+  group { 'puppet':
+    ensure => present,
+  }
+  # Prevent errors by running apt-get update when anything beneath /etc/apt/ changes
+  exec { "apt-get-update":
+   command => "/usr/bin/apt-get update",
+  }
+}
+
+class vagrantvm {
+  $mysqlpassword = ''
+  # passwords must always be actual hashes as used on disk in /etc/shadow
+  $webadminpassword = '$6$bCysFszn$Ycq5zFTrRm8QOWKWJk9qAr94okYBVLiQm/tcWe9dT6ohuFl46i8DoWP.LMdYJG7dQyFJh0XgDGeiWhnHUHLKF.'
+  $rootpassword = $webadminpassword
+  $backuppassword = $webadminpassword
+  include zivtechbase
+  include stylomatic
+  include solr
+  include php53-dev
+  class { 'mysql5':
+    mysqlpassword => $mysqlpassword
+  }
+}
+
+include setup
+include vagrantvm
