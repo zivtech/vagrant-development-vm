efb14a923f12573bdebdcafe8ee0b1d36a96bfb0
Howard Tyson <howard@howardtyson.com>
2012-08-03 08:35:58 -0400

    Updating drush fetcher with a drushrc template that provides services settings.

--- puppet-modules/drushfetcher/manifests/init.pp
+++ puppet-modules/drushfetcher/manifests/init.pp
@@ -1,26 +1,7 @@
-class ignitiondrush {
-
-  file { "/usr/share/drush":
-    ensure => directory,
-    owner => root,
-    group => root,
-    mode => 755,
-  }
-
-  file {  "/usr/share/drush/commands":
-    require => File['/usr/share/drush'],
-    ensure => directory,
-    owner => root,
-    group => root,
-    mode => 755,
-  }
-
-  vcsrepo { "/var/lib/drush":
-    ensure => present,
-    provider => git,
-    source => "http://git.drupal.org/project/drush.git",
-    revision => 'f3b05d6e4f40cb1bb86e75ed57272e21c0b8aee6',
-  }
+# Inherit from drush because we can't do anything without it
+# and because we want to ensure that the shared commands folder
+# exists and can't redeclare it.
+class drushfetcher ($fetcher_host) inherits drush {
 
   vcsrepo { "/usr/share/drush/commands/drush_fetcher":
     require => File["/usr/share/drush", "/usr/share/drush/commands"],
@@ -28,6 +9,29 @@ class ignitiondrush {
     provider => git,
     source => "http://git.drupal.org/sandbox/tizzo/1703334.git",
     revision => '94d635c',
+  }
+
+  vcsrepo { "/usr/share/drush/commands/fetcher_services":
+    require => File["/usr/share/drush", "/usr/share/drush/commands"],
+    ensure => present,
+    provider => git,
+    source => "http://git.drupal.org/sandbox/tizzo/1704332.git",
+    revision => '887d7c9060f87fd0c83c79cb7b938ad9507236fc',
+  }
+
+  file { "/etc/drush":
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => 755,
+  }
+
+  file { "/etc/drush/drushrc.php":
+    require => File['/etc/drush'],
+    mode => 755,
+    content => template("drushfetcher/drushrc.php.erb"),
+    owner => 'root',
+    group => 'root',
   }
 
 }
