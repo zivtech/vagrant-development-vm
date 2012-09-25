1b3a3c57f471238b44296e4d15e00a50feb18ec2
Laurence Liss <laurence@zivtech.com>
2012-09-25 10:31:20 -0400

    adding jenkins config

---
+++ puppet-modules/jenkins/manifests/init.pp
@@ -0,0 +1,38 @@
+class pound {
+  package { "jenkins":
+    ensure => installed,
+  }
+  service { "jenkins":
+    require => Package['jenkins'],
+    ensure => 'running',
+  }
+  file { '/etc/default/jenkins':
+    require => Package['pound'],
+    source => "puppet:///modules/jenkins/etc-default-jenkins",
+    owner => 'root',
+    group => 'root',
+    mode => 644,
+    notify => Service['jenkins'],
+  }
+  vcsrepo { '/var/lib/jekins':
+    require => Package['jenkins'],
+    source => "puppet:///modules//pound.cfg",
+    owner => 'root',
+    group => 'root',
+    mode => 644,
+    notify => Service['pound'],
+  }
+  vcsrepo { "/var/lib/jenkins":
+    require => [
+      Package['tomcat6'],
+      Package['jenkins'],
+    ],
+    source => "git@zivtech.unfuddle.com:zivtech/jenkinscfg.git",
+    ensure => "present",
+    provider => 'git',
+    notify => Service['jenkins'],
+    owner => 'jenkins',
+    group => 'root',
+    mode => 644,
+  }
+}
