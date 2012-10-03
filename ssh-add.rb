6bba7e0d8f40a4c6ba646683714437f99fdde48b
Howard Tyson <howard@howardtyson.com>
2012-10-03 19:20:39 -0400

    Working example.

--- ssh-add.rb
+++ ssh-add.rb
@@ -2,7 +2,7 @@ require 'vagrant'
 require 'net/ssh'
 
 module ZivtechKeyAdder
-  class Middleware
+  class Middleware# < Vagrant::Command::SSH
    def initialize(app, env, options = {})
       @app = app
       @env = env
@@ -11,33 +11,49 @@ module ZivtechKeyAdder
       puts 'initialize'
     end
 
+    def new
+      puts 'here'
+    end
+
     def execute
       agent = Net::SSH::Authentication::Agent.connect()
+      rsaFound = false
       agent.identities().each do |identity|
         if identity.comment.scan('id_rsa').length == 1
           # env[:ui].info "RSA key found, assuming it is safe for use."
-          return
+          rsaFound = true
         end
       end
       # If we are still running, no rsa key was found. Add one
       # env[:ui].info "No RSA key found, running ssh-add to add one."
-      Kernel.system "ssh-add"
+      if not rsaFound
+        Kernel.system "ssh-add"
+      end
+      sshCommand = Vagrant::Command::SSH.new @app, @env
+      sshCommand.execute()
     end
   end
 end
 
-module VagrantPlugins
-  module CommandFetcherSSH
-    class Command < Vagrant::Command::SSH
-      def execute
-        ZivtechKeyAdder::Middleware::call()
-        parent.execute()
-      end
-    end
-  end
+# ssh_mw = Vagrant::Action::Builder.new do
+#   use ZivtechKeyAdder::Middleware
+#   # use Vagrant::Command::SSH
+# end
+
+app = Vagrant::Action::Builder.new do
+  use ZivtechKeyAdder::Middleware
+  # use Vagrant::Command::SSH
 end
+Vagrant.commands.register(:ssh2) { Vagrant::Command::SSH }
 
 Vagrant.commands.register(:ssh) { ZivtechKeyAdder::Middleware }
+# Vagrant::Action.run(app)
+
+# Vagrant.commands.register :ssh, app
+# Vagrant.commands.register(:ssh) {
+#   ZivtechKeyAdder::Middleware
+#   Vagrant::Command::SSH
+# }
 
 # Vagrant.actions[:ssh].insert_before(1, ZivtechKeyAdder::Middleware)
 # puts Vagrant.commands[:ssh] =
