c0e6b090087652dd973dc132aeb7629cdb3788bd
Howard Tyson <howard@howardtyson.com>
2013-12-11 08:20:44 -0500

    Upgrading fetcher_services.

--- puppet-modules/drushfetcher/manifests/init.pp
+++ puppet-modules/drushfetcher/manifests/init.pp
@@ -3,12 +3,34 @@
 # exists and can't redeclare it.
 class drushfetcher ($fetcher_host) inherits drush {
 
+  include wget
+  wget::fetch { "Composer":
+    source      => 'http://getcomposer.org/composer.phar',
+    destination => '/usr/local/bin/composer',
+    timeout     => 0,
+    verbose     => false,
+  }->
+  file { '/usr/local/bin/composer':
+    mode => 755,
+  }
+
   vcsrepo { "/usr/share/drush/commands/drush_fetcher":
     require => File["/usr/share/drush", "/usr/share/drush/commands"],
     ensure => present,
     provider => git,
     source => "http://git.drupal.org/project/fetcher.git",
-    revision => 'b4da4f2dd64c7b2e6a7ffd4113866a37b31b607a',
+    revision => '411c545110fb03a45203c6fd8a4e4c8b3edc4df6',
+    notify   => Exec["/usr/bin/php /usr/local/bin/composer install"],
+  }
+
+  exec { "/usr/bin/php /usr/local/bin/composer install":
+    cwd     => "/usr/share/drush/commands/drush_fetcher",
+    creates => "/usr/share/drush/commands/drush_fetcher/node_modules",
+    require => [
+      Package['php5-cli'],
+      Wget::Fetch["Composer"],
+      Vcsrepo["/usr/share/drush/commands/drush_fetcher"],
+    ],
   }
 
   vcsrepo { "/usr/share/drush/commands/fetcher_services":
@@ -16,6 +38,7 @@ class drushfetcher ($fetcher_host) inherits drush {
     ensure => present,
     provider => git,
     source => "http://git.drupal.org/project/fetcher_services.git",
+    revision => "b1d56371b266c43cd76407df3765d0650140fdd6",
   }
 
   file { "/etc/drush":
