f7d61d812e2fc8ae93dc6d754f575b2bb86fce18
Jason Moore <sk33lz@gmail.com>
2015-10-21 10:17:00 -0700

    Fixing formatting/spacing.

--- custom-modules/phpmyadmin/manifests/init.pp
+++ custom-modules/phpmyadmin/manifests/init.pp
@@ -1,14 +1,14 @@
 class phpmyadmin {
 
- package { 'phpmyadmin':
-  ensure => 'installed',
- }
+  package { 'phpmyadmin':
+    ensure => 'installed',
+  }
 
- file { "/etc/apache2/sites-available/phpmyadmin.conf":
-  owner => root,
-  group => root,
-  mode => 644,
-  ensure => file,
-  source => "puppet:///modules/phpmyadmin/phpmyadmin.conf",
- }
+  file { "/etc/apache2/sites-available/phpmyadmin.conf":
+    owner => root,
+    group => root,
+    mode => 644,
+    ensure => file,
+    source => "puppet:///modules/phpmyadmin/phpmyadmin.conf",
+  }
 }
