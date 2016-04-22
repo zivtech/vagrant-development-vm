e816513b5e472cf19c2e5de827f07da1e4a69edb
Howard Tyson <howard@howardtyson.com>
2016-04-22 14:08:09 -0400

    Install Pantheon's terminus via composer.

---
+++ custom-modules/terminus/manifests/init.pp
@@ -0,0 +1,26 @@
+class terminus {
+
+  require php::composer
+
+  file { '/usr/share/composer':
+    mode  => '0555',
+    owner => 'root',
+    group => 'root',
+  }->
+
+  exec { 'Install terminus':
+    environment => [
+      'COMPOSER_HOME=/usr/share/composer',
+    ],
+    command     => '/usr/local/bin/composer global require pantheon-systems/terminus',
+    creates     => '/usr/share/composer/vendor/bin/terminus',
+  }->
+
+  file { '/usr/local/bin/terminus':
+    target => '/usr/share/composer/vendor/bin/terminus',
+    mode   => '0555',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+}
