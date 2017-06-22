12719ab33ce4e44f4b7a638b60d0355b378ce7e8
Laurence Liss <laurence@zivtech.com>
2017-06-22 09:31:08 -0400

    Fix issue in which preform module is loaded.
    We want to use apache evented mode. But PhpMyAdmin automatcially
    loads the prefork module and thus causes issues with our other
    provisioning. Here we ensure those modules are disabled after
    installing the pacakge.

--- custom-modules/phpmyadmin/manifests/init.pp
+++ custom-modules/phpmyadmin/manifests/init.pp
@@ -2,13 +2,37 @@ class phpmyadmin {
 
   package { 'phpmyadmin':
     ensure => 'installed',
-  }
+  }->
 
-  file { "/etc/apache2/sites-available/phpmyadmin.conf":
+  file { '/etc/apache2/mods-enabled/mpm_prefork.conf':
+    ensure  => 'absent',
+    require => Package['httpd'],
+    notify => Class['Apache::Service'],
+  }->
+
+  file { '/etc/apache2/mods-enabled/mpm_prefork.load':
+    ensure  => 'absent',
+    require => Package['httpd'],
+    notify => Class['Apache::Service'],
+  }->
+
+  file { '/etc/apache2/mods-enabled/php7.0.load':
+    ensure  => 'absent',
+    require => Package['httpd'],
+    notify => Class['Apache::Service'],
+  }->
+
+  file { '/etc/apache2/mods-enabled/php7.0.conf':
+    ensure  => 'absent',
+    require => Package['httpd'],
+    notify => Class['Apache::Service'],
+  }->
+
+  file { '/etc/apache2/sites-available/phpmyadmin.conf':
     owner => root,
     group => root,
     mode => '0644',
     ensure => file,
-    source => "puppet:///modules/phpmyadmin/phpmyadmin.conf",
+    source => "puppet:///modules/${module_name}/phpmyadmin.conf",
   }
 }
